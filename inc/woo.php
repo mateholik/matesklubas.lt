632f12ab387b41aa54fba92f79e8ea73a47e8bbb
user <user@users-MacBook-Pro.local>
2020-09-07 19:08:32 +0300

    comment on product

--- inc/woo.php
+++ inc/woo.php
@@ -11,11 +11,11 @@ function removeSorting() {
 //    unset( $tabs['description'] );
 //}
 
-add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
-function sb_woo_remove_reviews_tab($tabs) {
-    unset($tabs['reviews']);
-    return $tabs;
-}
+// add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
+// function sb_woo_remove_reviews_tab($tabs) {
+//     unset($tabs['reviews']);
+//     return $tabs;
+// }
 
 
 //woo checkout page
