0cd9c78517cb5898dd19624748860d13b3f574a5
mateholik <vaisov.vladimir@gmail.com>
2020-12-12 20:25:46 +0200

    gift card product

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -21,35 +21,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 ?>
-<div class="product_meta customMeta">
+<?php
+if($product->get_id() != 1067) { ?>
 
-	<?php do_action( 'woocommerce_product_meta_start' ); ?>
+    <div class="product_meta customMeta">
 
-	<?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
+        <?php do_action( 'woocommerce_product_meta_start' ); ?>
 
-		<span class="sku_wrapper"><?php esc_html_e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__( 'N/A', 'woocommerce' ); ?></span></span>
+        <?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
 
-	<?php endif; ?>
+            <span class="sku_wrapper"><?php esc_html_e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__( 'N/A', 'woocommerce' ); ?></span></span>
 
-	<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', count( $product->get_category_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+        <?php endif; ?>
 
-	<?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+        <?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', count( $product->get_category_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
 
-    <?php
-    productCustomTags($product->get_id(), 'salis', 'Kilmės šalis');
-    productCustomTags($product->get_id(), 'sudetis', 'Sudėtis');
-    productCustomTags($product->get_id(), 'skonio_intensyvumas', 'Skonio intensyvumas');
-    productCustomTags($product->get_id(), 'kiekis', 'Kiekis');
-    productCustomTags($product->get_id(), 'paruosimo_budas', 'Rekomenduojamas paruošimo būdas');
-    productCustomTags($product->get_id(), 'turis', 'Tūris');
-    productCustomTags($product->get_id(), 'aukstis', 'Aukštis');
-    productCustomTags($product->get_id(), 'skersmuo', 'Skersmuo');
-    productCustomTags($product->get_id(), 'medziaga', 'Medžiaga');
-    productCustomTags($product->get_id(), 'stilius', 'Stilius');
-    productCustomTags($product->get_id(), 'ilgis', 'Ilgis');
-    productCustomTags($product->get_id(), 'filtras', 'Filtras');
-    ?>
+        <?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
 
-	<?php do_action( 'woocommerce_product_meta_end' ); ?>
+        <?php
+        productCustomTags($product->get_id(), 'salis', 'Kilmės šalis');
+        productCustomTags($product->get_id(), 'sudetis', 'Sudėtis');
+        productCustomTags($product->get_id(), 'skonio_intensyvumas', 'Skonio intensyvumas');
+        productCustomTags($product->get_id(), 'kiekis', 'Kiekis');
+        productCustomTags($product->get_id(), 'paruosimo_budas', 'Rekomenduojamas paruošimo būdas');
+        productCustomTags($product->get_id(), 'turis', 'Tūris');
+        productCustomTags($product->get_id(), 'aukstis', 'Aukštis');
+        productCustomTags($product->get_id(), 'skersmuo', 'Skersmuo');
+        productCustomTags($product->get_id(), 'medziaga', 'Medžiaga');
+        productCustomTags($product->get_id(), 'stilius', 'Stilius');
+        productCustomTags($product->get_id(), 'ilgis', 'Ilgis');
+        productCustomTags($product->get_id(), 'filtras', 'Filtras');
+        ?>
 
-</div>
+        <?php do_action( 'woocommerce_product_meta_end' ); ?>
+    </div>
+<?php } ?>
+
