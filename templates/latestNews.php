ead30110c8585f9181f90e9911cecfc5a3c2eebf
Vladis macbook <vladuxs1@gmail.com>
2022-11-11 21:45:55 +0200

    news section from admin

--- templates/latestNews.php
+++ templates/latestNews.php
@@ -1,10 +1,16 @@
 <?php
-    $latestPost = wp_get_recent_posts(array(
-        'numberposts' => 1, // Number of recent posts thumbnails to display
+    $latestPosts = wp_get_recent_posts(array(
+        'numberposts' => 10, // Number of recent posts thumbnails to display
         'post_status' => 'publish', // Show only the published posts
         'category__not_in' => array(112)
+    ));
+    $latestVideos = wp_get_recent_posts(array(
+      'post_type'=>'videos',
+      'numberposts' => 10, // Number of recent posts thumbnails to display
+      'post_status' => 'publish' // Show only the published posts
     ))
-  ;?>
+
+  ?>
 
 <section class="news">
   <div class="container">
@@ -15,217 +21,37 @@
       <div class="block">
         <h3>video</h3>
         <div class="block__items">
-          <div class="block__card">
+          <?php foreach ($latestVideos as $item) { ?>
+          <a href="<?php echo get_permalink($item['ID']) ?>" class="block__card">
             <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
+              <?php echo get_the_post_thumbnail($item['ID'], 'home-news'); ?>
               <div class="icon">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
               </div>
             </div>
             <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
+              <p> <?php echo $item['post_title'] ?> </p>
               <span>plačiau »</span>
             </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-
+          </a>
+          <?php } ?>
         </div>
       </div>
       <div class="block">
         <h3>Straipsniai</h3>
         <div class="block__items">
-          <div class="block__card">
+          <?php foreach ($latestPosts as $item) { ?>
+          <a href="<?php echo get_permalink($item['ID']) ?>" class="block__card">
             <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
+              <?php echo get_the_post_thumbnail($item['ID'], 'home-news'); ?>
             </div>
             <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
+              <p> <?php echo $item['post_title'] ?> </p>
               <span>plačiau »</span>
             </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-          <div class="block__card">
-            <div class="image">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/placeholder-news.jpg" alt="thumb">
-              <div class="icon">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="icon">
-              </div>
-            </div>
-            <div class="content">
-              <p>Naujas būdas ruošti matė arbatą pridedant kitų žolelių pridedant kitų žolelių </p>
-              <span>plačiau »</span>
-            </div>
-          </div>
-
+          </a>
+          <?php } ?>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-<section>
-  <div class="news">
-    <div class="row">
-      <div class="row__col">
-        <?php
-                    $latestPost = wp_get_recent_posts(array(
-                        'numberposts' => 1, // Number of recent posts thumbnails to display
-                        'post_status' => 'publish', // Show only the published posts
-                        'category__not_in' => array(112)
-                    ))
-                    ;?>
-        <a href="<?php echo get_permalink($latestPost[0]['ID']) ?>" class="news__block">
-          <div class="news__preview">
-            <?php echo get_the_post_thumbnail($latestPost[0]['ID'], 'home-news'); ?>
-            <p class="news__category">Naujausias Straipsnis</p>
-            <div class="news__category-bg">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-cat-item.svg" alt="text-wrap">
-            </div>
-          </div>
-          <div class="news__text">
-            <h1 class="news__text-title"><?php echo $latestPost[0]['post_title'] ?></h1>
-            <p class="news__text-more">Plačiau »</p>
-          </div>
-          <div class="news__border"></div>
-        </a>
-      </div>
-      <div class="row__col">
-        <?php
-                    $latestVideo = wp_get_recent_posts(array(
-                        'post_type'=>'videos',
-                        'numberposts' => 1, // Number of recent posts thumbnails to display
-                        'post_status' => 'publish' // Show only the published posts
-                    ))
-                    ;?>
-        <a href="<?php echo get_permalink($latestVideo[0]['ID']) ?>" class="news__block">
-          <div class="news__preview">
-            <?php echo get_the_post_thumbnail($latestVideo[0]['ID'], 'home-news'); ?>
-            <p class="news__category">Naujausias vaizdo įrašas</p>
-            <div class="news__category-bg">
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-cat-item.svg" alt="text-wrap">
-            </div>
-            <div class="news__play">
-              <div class="news__play-wrap">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/play.svg" alt="play">
-              </div>
-            </div>
-          </div>
-          <div class="news__text">
-            <h1 class="news__text-title"><?php echo $latestVideo[0]['post_title'] ?></h1>
-            <p class="news__text-more">Plačiau »</p>
-          </div>
-          <div class="news__border"></div>
-        </a>
       </div>
     </div>
   </div>
