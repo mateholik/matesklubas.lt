280242ddcfd59ed3b8312221bb6b6df960375791
Vladis macbook <vladuxs1@gmail.com>
2022-12-26 14:40:46 +0200

    update facebook

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,8 +1,6 @@
 <div class="main__sidebar">
   <div class="sidebar">
-    <div class="sidebar__btn">
-      <a href="https://matesklubas.lt/negali-issirinkti/" class="btn">ATLIK TESTĄ</a>
-    </div>
+    <a href="https://matesklubas.lt/negali-issirinkti/" class="sidebar__btn">ATLIK TESTĄ</a>
     <div class="sidebar__block">
       <a href="https://matesklubas.lt/produkto-kategorija/mate/" class="sidebar__block-title">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/daigelis.svg" alt="daigelis">
@@ -49,9 +47,9 @@
       <div class="sidebar__block-title title-for-fb">
         Sekite naujienas Facebook'e:
       </div>
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmatesklubass&tabs=timeline&width=320&height=100&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-        width="320" height="100" style="border:none;overflow:hidden" scrolling="no" frameborder="0" target="_top"
+      <iframe target="_top"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmatesklubass&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=969696006719798"
+        width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
         allowfullscreen="true"
         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
