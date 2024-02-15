76df154c517b710ce742884bf3eade8d5a60b2e3
Vladis macbook <vladuxs1@gmail.com>
2024-02-15 11:14:05 +0200

    arrows to menu dropdown

--- inc/MkNavWalker.php
+++ inc/MkNavWalker.php
@@ -8,12 +8,12 @@ class mk_wp_nav_menu_walker extends Walker_Nav_menu {
     function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
         // echo '<pre>' . var_export($item, true) . '</pre>';
         if($item-> title == "Straipsniai") {
-            $output .= "<li class='blog' class='" .  implode(" ", $item->classes) . "'> <a href=" . esc_url($item-> url) . ">" . $item-> title . "</a>";
+            $output .= "<li class='blog' class='" .  implode(" ", $item->classes) . "'> <a href=" . esc_url($item-> url) . ">" . $item-> title . "<span class='arrow-down'></a>";
             $output .= "<div class='dropdown blog-dropdown'>";
             $output .= "<div class='inner-wrapper'>";
             $output .= "<div class='content'><div class='title'>Straipsnių kategorijos</div>";
         } else if($item-> title == "Parduotuvė") {
-            $output .= "<li class='store' class='" .  implode(" ", $item->classes) . "' > <a href=" . esc_url($item-> url) . ">" . $item-> title . "</a>";
+            $output .= "<li class='store' class='" .  implode(" ", $item->classes) . "' > <a href=" . esc_url($item-> url) . ">" . $item-> title . "<span class='arrow-down'></span></a>";
             $output .= "<div class='dropdown store-dropdown'>";
             $output .= "<div class='inner-wrapper'>";
             $output .= "<div class='title'>Prekių kategorijos</div>";
