1644110d9e160e159525b8580ddeec3d61696415
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-09 09:29:50 +0300

    Refactor asset compilation and update paths
    - Removed compiled JavaScript files: main.js and product.js.
    - Updated functions.php to enqueue scripts from the new dist directory.
    - Modified mix-manifest.json to reflect new asset paths.
    - Adjusted webpack.mix.js to compile assets into the dist directory instead of compiled/js and compiled/css.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
-mix.js("assets/javascript/main.js", "assets/compiled/js");
+mix.js('assets/javascript/main.js', 'dist');
 
-mix.js("assets/javascript/product.js", "assets/compiled/js");
+mix.js('assets/javascript/product.js', 'dist');
 
-mix.sass("assets/sass/style.scss", "assets/compiled/css").options({
+mix.sass('assets/sass/style.scss', 'dist').options({
   processCssUrls: false,
 });
 // mix.sass("assets/sass/homepage/style.scss", "assets/compiled/css/homepage");
